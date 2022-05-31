1b4d85851b25503e12bebe4235e0bad308af6eea
mingli.zhang <mingli.zhang@seamoney.com>
2022-05-31 19:15:07 +0800

    Revert "add option"
    This reverts commit 4ea320130942426efde3fd060059a26ac8bb4e3a.

--- bank_config.py
+++ bank_config.py
@@ -1,7 +1,4 @@
 # encoding = utf-8
-import os
-import sys
-
 BankConfig = {
 	35806119: 1.8e9,
 	35806354: 1.8e9,
@@ -9,5 +6,3 @@ BankConfig = {
 	35806558: 5e9,
 
 }
-BASE_DIR = os.path.dirname(__file__)
-IS_MAC = sys.platform == 'darwin'
